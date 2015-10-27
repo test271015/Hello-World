73118e0031358496600cbe5bfc1c0f9736b5b4a5
test271015 <h1762280@trbvm.com>
2015-10-27 12:02:24 +0100

    First commit
    work plz

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int ende = 5;
+		for (int i = 0; i < ende; i++) {
+			System.out.println(i + 1 + ". Hello World");
+		}
+		System.out.println("Ende");
+		
+	}
+}
